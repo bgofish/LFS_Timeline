389a904f603a6069104537cfd123f764cd7a4b24
bb6 <73919236+bgofish@users.noreply.github.com>
2026-04-01 19:47:33 +1300

    Update training_render.py

--- panels/training_render.py
+++ panels/training_render.py
@@ -1,12 +1,15 @@
-import lichtfeld
+import lfs
 
-class KeyframeSpreadsheetPanel(lichtfeld.types.Panel):
+def setup_data_binding():
+    # Placeholder for your data binding logic
+    pass
+
+class KeyframeSpreadsheetPanel(lfs.types.Panel):
     id = "TIMELINE_PT_spreadsheet"
     label = "Keyframe Spreadsheet"
-    # Point this to the .rml file in your plugin folder
-    panel_path = "training_render.rml"
+    # Make sure this .rml file exists in your plugin folder
+    panel_path = "training_render.rml" 
 
     def draw(self):
-        # This draw method can be left empty if you are using 
-        # RML for the entire UI layout.
+        # UI is handled by RML, but this method is required by the API
         pass
